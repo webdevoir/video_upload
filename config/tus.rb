8b721ef4a8268fb5bbfebfbb325b8c0b0f6b61ca
Sam <sam.dealy@gmail.com>
2018-06-22 14:37:30 -0400

    Add tus config

--- config/tus.rb
+++ config/tus.rb
@@ -1,1 +1,8 @@
 require "tus/storage/s3"
+
+Tus::Server.opts[:storage] = Tus::Storage::S3.new(
+  bucket:            "8ball-tv-dev", # required
+  access_key_id:     "AKIAIG3M2QCAUQSG2ROA",
+  secret_access_key: "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",
+  region:            "us-east-1",
+)
