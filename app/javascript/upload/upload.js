5de34b8e130d8386d8935fe6061246b7d8c28de7
Sam <sam.dealy@gmail.com>
2018-09-05 19:59:05 -0400

    Update uppy for multi-part

--- app/javascript/upload/upload.js
+++ app/javascript/upload/upload.js
@@ -19,26 +19,22 @@ const upload = {
           target: imagePreview.parentNode,
         });
 
-      uppy.use(Uppy.AwsS3, {
-       getUploadParameters: function (file) {
-         return fetch('/presign?filename=' + file.name, { // Shrine's presign endpoint
-           credentials: 'same-origin', // send cookies
-         }).then(function (response) { return response.json() })
-       }
-     });
+      uppy.use(Uppy.AwsS3Multipart, {
+        serverUrl: ENV["APP-URL"],
+      });
 
       uppy.on('upload-success', function (file, data, uploadURL) {
-     // show image preview
-     imagePreview.src = URL.createObjectURL(file.data)
-      // construct uploaded file data in the format that Shrine expects
-      var uploadedFileData = JSON.stringify({
-        id: uploadURL.match(/\/cache\/([^\?]+)/)[1], // extract key without prefix
-        storage: 'cache',
-        metadata: {
-          size:      file.size,
-          filename:  file.name,
-          mime_type: file.type,
-        }
+        // show image preview
+        imagePreview.src = URL.createObjectURL(file.data)
+        // construct uploaded file data in the format that Shrine expects
+        var uploadedFileData = JSON.stringify({
+          id: uploadURL.match(/\/cache\/([^\?]+)/)[1], // extract key without prefix
+          storage: 'cache',
+          metadata: {
+            size:      file.size,
+            filename:  file.name,
+            mime_type: file.type,
+          }
       });
 
      // set hidden field value to the uploaded file data so that it's submitted with the form as the attachment
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root'video#new'
   resources :video, only: [:create, :new]
 
-  mount Shrine.presign_endpoint(:cache), at: '/presign'
+   mount Shrine.uppy_s3_multipart(:cache) => "/s3"
 end
