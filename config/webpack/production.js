fbefe99ebf38bf4d7ba361fe65a94a02b83070e8
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2018-10-03 08:39:30 -0500

    Ignore externals in production webpack config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+const environment = require('./environment');
+const nodeExternals = require('webpack-node-externals');
+environment.config.externals = [nodeExternals()];
 
 module.exports = environment.toWebpackConfig()
